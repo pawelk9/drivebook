b07e53e6cfc7e5b44f2916a61c6e3bc76702134e
Pawel <pawelk9@gmail.com>
2015-08-30 11:33:01 +0200

    Formatting admin form

--- map/admin.py
+++ map/admin.py
@@ -4,11 +4,20 @@ from .models import SpeedCamera
 
 
 class SpeedCameraAdmin(geoadmin.GeoModelAdmin):
+
+    fieldsets = [
+        ('Speed device', {'fields': ['type', 'address']}),
+        ('Address',
+         {'fields': ['road', 'city', 'country', 'postcode', 'state', 'house_number', 'county', 'suburb']}),
+        ('Way', {'fields': ['ref', 'max_speed']}),
+        ('Geography', {'fields': ['geometry', 'longtitude', 'lattitude']}),
+        ('Open Street Map', {'fields': ['osm_url']}),
+        ('Date', {'fields': ['created_date']}),
+        ('Other', {'fields': ['note']}),
+    ]
+
     list_display = ('type', 'geometry', 'city', 'road', 'ref', 'state', 'created_date')
     list_filter = ('created_date', 'type', 'city', 'state', 'ref')
-    #search_fields = ['name']
+
 
 geoadmin.site.register(SpeedCamera, SpeedCameraAdmin)
-
-
-
